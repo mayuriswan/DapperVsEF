d4d06b4b3d58dc46f8841e3eebd7495d744384cc
Ayoub El haoussi <aelhaoussi@sqli.com>
2024-06-02 15:34:07 +0100

    first

---
+++ CompanyApi/Controllers/CompanyController.cs
@@ -0,0 +1,74 @@
+﻿using CompanyApi.Data;
+using CompanyApi.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CompanyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly DataBaseContext _dbContext ;
+
+        public CompanyController(DataBaseContext dbContext)
+        {
+            _dbContext = dbContext ;
+        }
+
+        [HttpPut("/increase-salaries")]
+        public async Task<IActionResult> IncreaseSalaries(int comapnyId)
+        {
+            var company = await _dbContext
+                                          .Set<Company>()
+                                          .Include(c=>c.Employees)
+                                          .FirstOrDefaultAsync(c=>c.Id==comapnyId);
+            if(company == null)
+            {
+                return  NotFound($"this comapny with Id {comapnyId} doesnt exist ");
+            }
+            foreach(var employee in company.Employees)
+            {
+                employee.Salary += employee.Salary * 0.1m;
+            }
+            await _dbContext.Database.BeginTransactionAsync();
+            await _dbContext.Database.ExecuteSqlInterpolatedAsync($"Update Employees Set SALARY = Salary*1.1 WHERE CompanyId = {comapnyId}");
+            company.LastSalaryUpdate= DateTime.Now;
+            await _dbContext.SaveChangesAsync();
+            await _dbContext.Database.CommitTransactionAsync();
+            return  NoContent();
+
+        }
+        [HttpPut("/increase-salaries-dapper")]
+        public async Task<IActionResult> IncreaseSalariesDapper(int comapnyId)
+        {
+            var company = await _dbContext
+                                          .Set<Company>()
+                                          .Include(c => c.Employees)
+                                          .FirstOrDefaultAsync(c => c.Id == comapnyId);
+            if (company == null)
+            {
+                return NotFound($"this comapny with Id {comapnyId} doesnt exist ");
+            }
+            foreach (var employee in company.Employees)
+            {
+                employee.Salary += employee.Salary * 0.1m;
+            }
+            var transaction = await _dbContext.Database.BeginTransactionAsync();
+            await _dbContext.Database.GetDbConnection().ExecuteAsync(
+                "Update Employees Set SALARY = Salary*1.1 WHERE CompanyId = @companyId",
+                new {companyId = company.Id},
+                transaction.GetDbTransaction()
+                );
+            company.LastSalaryUpdate = DateTime.Now;
+            await _dbContext.SaveChangesAsync();
+            await _dbContext.Database.CommitTransactionAsync();
+            return NoContent();
+
+        }
+
+    }
+}
---
+++ CompanyApi/Data/DataBaseContext.cs
@@ -0,0 +1,47 @@
+﻿using CompanyApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyApi.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options):base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>(builder =>
+            {
+                builder.ToTable("Companies");
+                builder
+                       .HasMany(company => company.Employees)
+                       .WithOne()
+                       .HasForeignKey(employee => employee.CompanyId)
+                       .IsRequired();
+                builder
+                    .HasData(new Company
+                    {
+                        Id = 1,
+                        Name = "Mayuri",
+
+                    });
+                
+
+            });
+            modelBuilder.Entity<Employee>(builder =>
+            {
+                builder.ToTable("Employees");
+                var employees = Enumerable
+                                    .Range(1, 1000)
+                                    .Select(id => new Employee
+                                    {
+                                        Id = id,
+                                        Name = $"Employee #{id}",
+                                        CompanyId = 1,
+                                        Salary=100
+
+                                    });
+                builder.HasData(employees);
+            });
+        }
+    }
+}
